ebf6025e0ffecd5543b2011468b021859532fc8f
Kristina Spasevska <spasevska.kristina87@gmail.com>
2020-03-02 22:22:26 +0100

    g4 class01 added

---
+++ g4/Class01/ConsoleApp-Class01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Class01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Test string");
+
+            // Console.Writeline writes in console leaving a new line after
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("We are learning C#");
+
+            // Console.Write writes in console and does not leave a line after
+            Console.Write("Dejan ");
+            Console.Write("Kristina ");
+
+            // Console.Readline reads from console
+            Console.ReadLine();
+        }
+    }
+}
