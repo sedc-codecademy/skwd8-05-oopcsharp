4bb82038df836b90a260674f176bd47b041f19e3
GK0208 <goce.kabov@yahoo.com>
2020-03-04 09:32:49 +0100

    updated .netCore consoleApp

---
+++ g1/Class01/SEDC_First_Class/ExampleSolution/ConsoleApp.NetCore/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp.NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We use console writeln when we want to print something on the console
+
+            Console.WriteLine("       *        ");
+            Console.WriteLine("      * *       ");
+            Console.WriteLine("     * * *      ");
+            Console.WriteLine("    * * * *     ");
+            Console.WriteLine("  * * * * * *   ");
+
+            Console.WriteLine("  ******       *   ");
+            Console.WriteLine("  *         *  *   ");
+            Console.WriteLine("  *       *    *   ");
+            Console.WriteLine("  *            *   ");
+            Console.WriteLine("  *            *   ");
+
+            // We use console readline when we expect some input from the user,
+            // or to stop our console application at specific point like in this example
+
+            Console.ReadLine();
+        }
+    }
+}
