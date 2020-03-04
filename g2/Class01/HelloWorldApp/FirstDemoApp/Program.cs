2078b2b5c70562dda55b8e803d090fd3039c7562
Martin Panovski <martinp@iborn.net>
2020-03-04 21:39:38 +0100

    Added class 01 code and README.md

---
+++ g2/Class01/HelloWorldApp/FirstDemoApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace FirstDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Martin Panovski!");
+            Console.WriteLine("Hi we are learning C#, and it is so cool!");
+
+            //Exercise 1
+            //Console.WriteLine("     *     ");
+            //Console.WriteLine("    ***    ");
+            //Console.WriteLine("   *****   ");
+            //Console.WriteLine("  *******  ");
+            //Console.WriteLine(" ********* ");
+            //Console.WriteLine("    | |    ");
+
+            Console.WriteLine("============================");
+
+            //Exercise 2
+            Console.WriteLine("    *   ");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("    *   ");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("   ***  ");
+            Thread.Sleep(2000);
+
+            Console.WriteLine(" *  *  *");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("    *  ");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("    *  ");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("  *   *");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("*       *");
+
+            
+            Console.ReadLine();     
+        }
+    }
+}
