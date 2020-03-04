b1792a406d7d758e1b1899854aa033364861bb8e
GK0208 <goce.kabov@yahoo.com>
2020-03-04 01:17:29 +0100

    added examples from first class

---
+++ g1/Class01/SEDC_First_Class/ExampleSolution/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We use console writeln when we want to print something on the console
+            Console.WriteLine("Hello G1!");
+
+            // We use console readline when we expect some input from the user,
+            // or to stop our console application at specific point like in this example
+            Console.ReadLine();
+        }
+    }
+}
