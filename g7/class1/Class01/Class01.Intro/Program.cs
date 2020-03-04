b81b324245a64348793b774f1ad6ab0243ccf057
Dragan Gelevski <hello@dragan.codes>
2020-03-04 11:37:28 +0100

    Class 1 finito

---
+++ g7/class1/Class01/Class01.Intro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Class01.Intro
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// YOUR CODE GOES HERE
+			// Console.WriteLine:
+			// Write Text and then add a new Line
+			Console.WriteLine("Hello World!");
+			Console.WriteLine("Hello from SEDC!");
+			// Console.Write:
+			// Write Text and don't add a new line
+			Console.Write("This is with Write!");
+			Console.Write("Hello from SEDC but Write!");
+			Console.ReadLine();
+		}
+	}
+}
