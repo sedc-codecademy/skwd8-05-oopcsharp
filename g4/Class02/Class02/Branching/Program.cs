e69ee6dc51b3baf602ef7a6b2fc93ff69ec8875d
Dejan <djovanov92@gmail.com>
2020-03-06 22:21:54 +0100

    Class 02 Code added

---
+++ g4/Class02/Class02/Branching/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Excercises 5-7
+            // Excercise 5
+            //int numOfApples = 8;
+            //int basketCapacity = 5;
+
+            //Console.WriteLine("Please enter number of threes");
+            //string userInput = Console.ReadLine();
+
+            //int numOfThrees = int.Parse(userInput);
+
+            //int totalApples = numOfApples * 12 * numOfThrees;
+
+            //int result = 0;
+            //if (totalApples % basketCapacity == 0)
+            //{
+            //    result = totalApples / basketCapacity;
+            //} else
+            //{
+            //    result = (totalApples / basketCapacity) + 1;
+            //}
+
+            //Console.WriteLine("You would need " + result + 
+            //    " baskets for the " + totalApples + " apples");
+
+
+
+            // Excercise 6
+            //Console.WriteLine("Please enter the first number: ");
+            //int firstNum = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Please enter the second number: ");
+            //int secondNum = int.Parse(Console.ReadLine());
+
+            //// check which is larger
+            //int larger;
+            //if (firstNum > secondNum)
+            //{
+            //    larger = firstNum;
+            //    Console.WriteLine("The first number is larger!");
+            //} else if (secondNum > firstNum)
+            //{
+            //    larger = secondNum;
+            //    Console.WriteLine("The second number is larger!");
+            //} else
+            //{
+            //    larger = firstNum;
+            //    Console.WriteLine("The numbers are equal!");
+            //}
+
+            //// check if it is even or odd
+            //if (larger != 0)
+            //{
+            //    if (larger % 2 == 0)
+            //    {
+            //        Console.WriteLine("The Number is Even!");
+            //    }
+            //    else if (larger % 2 != 0)
+            //    {
+            //        Console.WriteLine("The Number is Odd");
+            //    }
+            //} else
+            //{
+            //    Console.WriteLine("You entered Zero this is not even nore odd !");
+            //}
+
+            //// Excercise 7
+            //Console.WriteLine("Please enter number between 1 and 3");
+            //int userInput = int.Parse(Console.ReadLine());
+
+            //switch (userInput)
+            //{
+            //    case 1:
+            //        Console.WriteLine("You got a new car !");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("You got a new plane !");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("You got a new bike !");
+            //        break;
+            //    default:
+            //        Console.WriteLine("You have entered wrong number !");
+            //        break;
+            //}
+            #endregion
+        }
+    }
+}
