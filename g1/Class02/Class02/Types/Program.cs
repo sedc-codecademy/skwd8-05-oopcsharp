b95ef2d593f1ff63b6e31358218b5c2b2f1e63f5
GK0208 <goce.kabov@yahoo.com>
2020-03-06 13:21:34 +0100

    added class 02

---
+++ g1/Class02/Class02/Types/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declared and init integer variable
+            int number = 10;
+
+            // Declared and init short (int) variable
+            short shortNumber = 1;
+
+            // Declared and init long (int) variable
+            long longNumber = 10000000000000;
+
+            // Declared and init double variable
+            double doubleNumber = 10.10;
+
+            // Declared and init decimal variable
+            decimal decimalNumber = 10.10m;
+
+            // Declared and init float variable
+            float floatNumber = 10.20f;
+
+            // Difference between (float, double, decimal) and (int, short, long) is the size of the box 
+            // that we discussed on the class
+
+
+            // Declared and init byte variable
+            byte someByte = 250;
+
+            // Declared and init string variable
+            string text = "G1 Rocks !";
+
+            // Declared and init char variable
+            char singleCharacter = 'G';
+
+            // Declared and init boolean variable
+            bool answer = true;
+
+
+
+        }
+    }
+}
