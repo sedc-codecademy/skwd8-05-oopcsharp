c119498bbab9806c42aac9189216ff913ee4dbf4
Risto Panchevski <Risto.Pancevski@stas.be>
2020-03-24 22:58:13 +0100

    Class 03 Loops and arrays

---
+++ g5/Class03/Exe1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Exe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FOR Zdravo!");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i + ". Zdravo!");
+            }
+
+
+            Console.WriteLine("While Zdravo!");
+            int k = 0;
+            while (k < 10)
+            {
+                Console.WriteLine(k + ". Zdravo!");
+                k++;
+            }
+
+            Console.WriteLine("DO-While Zdravo!");
+            int t = 0;
+            do
+            {
+                Console.WriteLine(t + ". Zdravo!");
+                t++;
+            } while (t < 10);
+
+            Console.WriteLine("Break");
+            for (int j = 0; j < 10; j++)
+            {
+                if(j == 2)
+                    break;
+                
+                Console.WriteLine(j + ". Zdravo!");
+            }
+            
+            Console.WriteLine("Continue");
+            for (int u = 0; u < 10; u++)
+            {
+                if (u == 2)
+                    continue;
+
+                Console.WriteLine(u + ". Zdravo!");
+            }
+
+            Console.WriteLine("Kraj");
+        }
+    }
+}
---
+++ g5/Class03/Exe3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Exe3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+
+            var number1 = Console.ReadLine();
+
+            if (Int32.TryParse(number1, out numbers[0]))
+            {
+                var number2 = Console.ReadLine();
+                if (Int32.TryParse(number2, out numbers[1]))
+                {
+                    var number3 = Console.ReadLine();
+                    if (Int32.TryParse(number3, out numbers[2]))
+                    {
+                        int suma = 0;
+
+                        foreach (var number in numbers)
+                        {
+                            suma += number;
+                        }
+
+                        Console.WriteLine("Zbirot e: " + suma);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ne vnesovte broj!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ne vnesovte broj!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ne vnesovte broj!");
+            }
+        }
+    }
+}
