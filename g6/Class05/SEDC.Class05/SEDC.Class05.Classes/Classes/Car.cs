785be717103a6749d77b405eee715c8489681ce2
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2020-03-25 21:38:36 +0100

    Added class 05

---
+++ g6/Class05/SEDC.Class05/SEDC.Class05.Classes/Classes/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Class05.Classes.Classes
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public DateTime ProductionYear { get; set; }
+        public bool IsRunning { get; set; }
+
+        public void Start()
+        {
+            if (IsRunning)
+            {
+                Console.WriteLine("The car is running");
+            }
+            else
+            {
+                IsRunning = true;
+            }
+        }
+
+        public void Stop()
+        {
+            if (!IsRunning)
+            {
+                Console.WriteLine("The car is not running");
+            }
+            else
+            {
+                IsRunning = false;
+            }
+        }
+
+        public void Drive()
+        {
+            if (IsRunning)
+            {
+                Console.WriteLine("Car is driving.");
+            }
+            else
+            {
+                Console.WriteLine("You should first start the car in order to drive.");
+            }
+        }
+
+        public string GetInfo()
+        {
+            return $"{this.Brand} {Model} is manufatored in {ProductionYear}";
+        }
+    }
+}
---
+++ g6/Class05/SEDC.Class05/SEDC.Class05.Classes/Classes/Human.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Class05.Classes.Classes
+{
+    public class Human
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int SSN { get; set; }
+        private string FullName { get; set; }
+    }
+}
