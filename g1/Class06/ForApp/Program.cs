51d90b0f38e4b79de305d7bd22e8af60070aa04b
GK0208 <goce.kabov@yahoo.com>
2020-03-31 21:10:47 +0200

    added class 06

---
+++ g1/Class06/ForApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ForApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 10, 20, 10, 50, 80 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("For(i): " + numbers[i]);
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("For(j): " + numbers[j]);
+                }
+            }
+        }
+    }
+}
