c7e1778cc4bec2aaf526c823e3b4a443802ed8ab
dblazeski96 <dblazeski96@gmail.com>
2020-04-01 00:05:07 +0200

    Added code from classes

---
+++ g3/Class05/code-retrpospective/Classes/ClassesRetrospective/Models/Human.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesRetrospective.Models
+{
+    class Human
+    {
+        public Human() { }
+        public Human(string name, string lastName, int age, int accountNumber)
+        {
+            this.Name = name;
+            LastName = lastName;
+            Age = age;
+            AccountNumber = accountNumber;
+        }
+
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        private int AccountNumber { get; set; }
+    }
+}
