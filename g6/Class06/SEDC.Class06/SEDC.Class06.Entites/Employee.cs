b303e246a5eb582dca12107cc2da4a6e672f8e02
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2020-03-31 14:09:46 +0200

    added clkass 06

---
+++ g6/Class06/SEDC.Class06/SEDC.Class06.Entites/Employee.cs
@@ -0,0 +1,42 @@
+﻿using SEDC.Class06.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Class06.Entites
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Salary { get; set; }
+        public Role Role { get; set; }
+        public DateTime CreateOn { get; set; }
+
+        public Employee()
+        {
+            CreateOn = DateTime.Now;
+        }
+
+        public Employee(string firstName, string lastName, double salary, Role role)
+        {
+            CreateOn = DateTime.Now;
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+            Role = role;
+        }
+
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            CreateOn = DateTime.Now;
+        }
+
+        public string PrintInfo()
+        {
+            return $"Fullname: {FirstName} {LastName} Role: {Role}";
+        }
+    }
+}
---
+++ g6/Class06/SEDC.Class06/SEDC.Class06.Enumerations/DaysOfWeek.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Class06.Enumerations
+{
+    public enum DaysOfWeek
+    {
+        Monday = 1,
+
+    }
+}
