3cf4762b9f3b02a24d155530d7fe6f9b8149aae2
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2020-04-01 20:24:03 +0200

    added class 6 excersises

---
+++ g6/Class06/SEDC.Class06.Excerisises/SEDC.Class06.Excersises.App/Program.cs
@@ -0,0 +1,37 @@
+﻿using SEDC.Class06.Excersises.Business.Services;
+using System;
+
+namespace SEDC.Class06.Excersises.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ValidationSevice validationSevice = new ValidationSevice();
+            var validationSevice = new ValidationSevice();
+
+            string email = Console.ReadLine();
+            if (!validationSevice.ValidateEmail(email))
+            {
+                Console.WriteLine("Enter valid email.");
+            }
+            else
+            {
+                Console.WriteLine("Great it works");
+            }
+
+            string password = Console.ReadLine();
+            if (!validationSevice.ValidatePassword(password))
+            {
+                Console.WriteLine("Password is not valid, should be minimum 8 chars 1 upper letter and 1 digit");
+            }
+            else
+            {
+                Console.WriteLine("Super it works");
+            }
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ g6/Class06/SEDC.Class06.Excerisises/SEDC.Class06.Excersises.Business/Services/ValidationSevice.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Class06.Excersises.Business.Services
+{
+    public class ValidationSevice
+    {
+        public bool ValidateEmail(string email)
+        {
+            if (!email.Contains('@'))
+            {
+                return false;
+            }
+
+            // "trajanstevkovski@gmail.com" -> Split '@' ->  [ "trajanstevkovski", "gmail.com" ]
+            string[] emailParts = email.Split('@');
+            if(emailParts.Length != 2)
+            {
+                return false;
+            }
+
+            if(emailParts[emailParts.Length - 1].Length <= 6
+                && !emailParts[emailParts.Length - 1].Contains('.'))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidatePassword(string password)
+        {
+            // min length 8 char, min 1 upper letter, min 1 digit
+            if(password.Length < 8)
+            {
+                return false;
+            }
+
+            char[] passwordParts = password.ToCharArray();
+
+            int countUpperLetters = 0;
+            int countDigits = 0;
+            foreach (char part in passwordParts)
+            {
+                if (char.IsWhiteSpace(part))
+                {
+                    return false;
+                }
+
+                if (char.IsUpper(part))
+                {
+                    countUpperLetters++;
+                }
+
+                if (char.IsDigit(part))
+                {
+                    countDigits++;
+                }
+            }
+
+            return countUpperLetters != 0 && countDigits != 0;
+        }
+    }
+}
