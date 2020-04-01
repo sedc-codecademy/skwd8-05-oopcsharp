f42e4be6b2ec110138714fb1ac21a98dc53df546
Martin Panovski <martinp@iborn.net>
2020-04-01 21:00:24 +0200

    Class 5 code added

---
+++ g2/Class05/SEDC.ClassesPart_1/SEDC.ClassesPart_1/Models/Dog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.ClassesPart_1.Models
+{
+    public class Dog
+    {
+        public string Name;
+        public string Sort;
+        public string Color;
+
+        public void Bark()
+        {
+            Console.WriteLine("Woof woof!!!");
+        }
+    }
+}
