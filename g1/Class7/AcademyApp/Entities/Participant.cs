6efd4024c4f965a50b7b99cefdbcf6f8e98beb18
Miodrag <miodrag.cekikj@seavus.com>
2020-04-02 18:39:22 +0200

    Class 7 On The Fly
    - Add solution with Classes project for the initial code base needed for class 7

---
+++ g1/Class7/AcademyApp/Entities/Participant.cs
@@ -0,0 +1,54 @@
+﻿using AcademyApp.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AcademyApp.Entities
+{
+    public class Participant
+    {
+        public Participant()
+        {
+
+        }
+
+        public Participant(string firstName)
+        {
+            FirstName = firstName;
+        }
+
+        public Participant(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        private int Age;
+
+        public string FirstName;
+        public string LastName;
+        public DateTime DateOfBirth;
+        public string[] Subjects;
+
+        public AcademyRole Role;
+
+        public void Greetings()
+        {
+            Console.WriteLine($"Hello, I`m {FirstName} {LastName}");
+
+            HowOld(DateTime.Today);
+            Console.WriteLine($"Also, I`m {Age} years old. :)");
+        }
+
+        private void HowOld(DateTime today)
+        {
+            if (today.Month < DateOfBirth.Month)
+                Age = today.Year - DateOfBirth.Year - 1;
+        }
+
+        public void PrintFullName()
+        {
+            Console.WriteLine($"{FirstName} {LastName}");
+        }
+    }
+}
