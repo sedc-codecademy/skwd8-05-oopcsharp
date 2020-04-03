70e191000e8cf9c66539facd1bd8f37f21b18398
dblazeski96 <dblazeski96@gmail.com>
2020-04-03 13:31:57 +0200

    Added code from class 6

---
+++ g3/Class06/code/Classes/Models/Action.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public enum Action
+    {
+        Eat = 0,
+        NotEat = 1,
+        Bark,
+        NotBark,
+        Tail,
+        NotTail
+    }
+}
