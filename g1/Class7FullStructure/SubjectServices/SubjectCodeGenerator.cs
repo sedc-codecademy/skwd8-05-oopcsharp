52274e5f78a04d79e08270964a234eacd17115c0
Miodrag <miodrag.cekikj@seavus.com>
2020-04-03 13:25:42 +0200

    Class 7 Full Application Version
    - Complete code base with structured solution and class library

---
+++ g1/Class7FullStructure/SubjectServices/SubjectCodeGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SubjectServices
+{
+    class SubjectCodeGenerator
+    {
+        public static int GenerateSubjectCode()
+        {
+            Random rand = new Random();
+
+            int code = rand.Next(0, 10);
+
+            return code;
+        }
+    }
+}
