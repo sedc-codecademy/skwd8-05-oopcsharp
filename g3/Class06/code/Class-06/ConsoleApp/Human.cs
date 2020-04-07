f86b031954f5d0a3468ff1cd0ebf8dcbafe96f74
dblazeski96 <dblazeski96@gmail.com>
2020-04-07 19:31:29 +0200

    Added initial code for class 6

---
+++ g3/Class06/code/Class-06/ConsoleApp/Human.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public string GetAge()
+        {
+            return $"{Age} years";
+        }
+    }
+}
