3e18beefec201215666db74e7e3e3ab670111bef
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2020-04-08 20:51:04 +0200

    Added app

---
+++ g6/Class07/SEDC.eStore/SEDC.eStore.Business/ApplicationMenu/Menu.cs
@@ -0,0 +1,22 @@
+﻿using SEDC.eStore.Entities.Models;
+using System;
+
+namespace SEDC.eStore.Business.ApplicationMenu
+{
+    public static class Menu
+    {
+        public static void WelcomeScreen()
+        {
+            Console.WriteLine("Welcome to eStore console app");
+            Console.WriteLine("Login to continue");
+        }
+
+        public static void PrintProducts(Product[] products)
+        {
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.Info());
+            }
+        }
+    }
+}
