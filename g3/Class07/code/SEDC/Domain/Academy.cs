c314eecf3525228e3fa1a3d06cfe3e24e220f391
dblazeski96 <dblazeski96@gmail.com>
2020-04-09 19:35:35 +0200

    Added Exercises Initail Solution

---
+++ g3/Class07/code/SEDC/Domain/Academy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Academy
+    {
+        public Academy(string name, string desc, Subject[] subjects)
+        {
+            Subjects = subjects;
+            IsStared = false;
+        }
+
+        public Subject[] Subjects { get; set; }
+        public bool IsStared { get; set; }
+    }
+}
---
+++ g3/Class07/code/SEDC/Services/AcademyService.cs
@@ -0,0 +1,45 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public class AcademyService
+    {
+        public void StartAcademy(Academy academy)
+        {
+
+        }
+
+        public void EndAcademy(Academy academy)
+        {
+
+        }
+
+        public void StartSubject(Academy academy, string subjectName)
+        {
+
+        }
+
+        public void EndSubject(Academy academy, string subjectName)
+        {
+
+        }
+
+        public void AddStudentToSubject(Subject subject, Student student)
+        {
+
+        }
+
+        public void AddSubjectToAcademy(Academy academy, Subject subject)
+        {
+
+        }
+
+        public Student CreateStudent(string name, string lastName, int age, int cardNumber)
+        {
+            return null; // Remove this line, it's here just as a placeholder so that the compiler doesn't give us errors
+        }
+    }
+}
