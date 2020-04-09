0780a96bbbd77774d95a00c5686dfdd77290da9f
dblazeski96 <dblazeski96@gmail.com>
2020-04-09 19:04:54 +0200

    Added solution for class 6 extra exercise

---
+++ g3/Class06/code/Excercise-solution/SEDC/DomainModels/SongModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainModels
+{
+    public class SongModel
+    {
+        public string Name { get; set; }
+        public string Band { get; set; }
+        public int Length { get; set; }
+        public Genre Genre { get; set; }
+        public string Author { get; set; }
+        public string Language { get; set; }
+    }
+}
---
+++ g3/Class06/code/Excercise-solution/SEDC/Services/SongService.cs
@@ -0,0 +1,66 @@
+﻿using DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public class SongService
+    {
+        public SongModel FindByName(SongModel[] songs, string name)
+        {
+            foreach (SongModel song in songs)
+            {
+                if (song.Name == name)
+                    return song;
+            }
+
+            return null;
+        }
+
+        //public void FindByName(SongModel[] songs, string name)
+        //{
+        //    foreach (SongModel song in songs)
+        //    {
+        //        if (song.Name == name)
+        //            Console.WriteLine($"{song.Name} {song.Author} {song.Length}s");
+        //    }
+        //}
+
+        public SongModel[] FindByAuthor(SongModel[] songs, string author)
+        {
+            return null;
+        }
+
+        public SongModel[] FindByGenre(SongModel[] songs, Genre genre)
+        {
+            return null;
+        }
+
+        public SongModel[] FindSongsLongerThan(SongModel[] songs, int seconds)
+        {
+            SongModel[] resultSongs = new SongModel[0];
+
+            foreach (SongModel song in songs)
+            {
+                if (song.Length > seconds)
+                {
+                    Array.Resize(ref resultSongs, resultSongs.Length + 1);
+                    resultSongs[resultSongs.Length - 1] = song;
+                }
+            }
+
+            return resultSongs;
+        }
+
+        public SongModel[] FindSongsShorterThan(SongModel[] songs, int seconds)
+        {
+            return null;
+        }
+
+        public SongModel[] AddSong(SongModel[] songs, SongModel book)
+        {
+            return null;
+        }
+    }
+}
