ca349d0bee436d9e204c5287d7276ab00ddff3c8
Martin Panovski <martinp@iborn.net>
2020-04-11 15:01:35 +0200

    Class07 part 1 added

---
+++ g2/Class07_Part1/SEDC.Inheritance/SEDC.Class07_Part1/Models/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Class07_Part1.Models
+{
+    public class Student : Participant
+    {
+        //Specific only for student classs
+        public int Group { get; set; }
+
+        public Student()
+        {
+
+        }
+
+        public Student(string firstName, string lastName)
+            :base(firstName, lastName)
+        {
+        }
+
+        public void FavouriteSubject(Subject subject)
+        {
+            switch (subject.Title)
+            {
+                case "C#Basic":
+                    Console.WriteLine("COVID-19 break exercises.");
+                    break;
+                case "AJS":
+                    Console.WriteLine("Star Wars app homework");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ g2/Class07_Part1/SEDC.Inheritance/SEDC.Class07_Part1/Models/WebDevStudent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Class07_Part1.Models
+{
+    public class WebDevStudent : Student
+    {
+        public WebDevStudent()
+        {
+            AccountBalance = 100;
+        }
+    }
+}
