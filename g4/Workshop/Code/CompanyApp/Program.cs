adf324f0fa265ca27fca8f6ca6ca149085148489
Kristina Spasevska <spasevska.kristina87@gmail.com>
2020-04-20 18:37:40 +0200

    workshop added

---
+++ g4/Workshop/Code/CompanyApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using CompanyLibrary.Models;
+using System;
+
+namespace CompanyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = new Manager("Bob", "Bobsky");
+            manager.PrintInfo();
+            manager.AddBonus(3000);
+            Console.WriteLine("Salary: {0}", manager.GetSalary());
+            Console.WriteLine("------------------------------------");
+
+            SalesPerson sales01 = new SalesPerson("John", "Snow");
+            sales01.PrintInfo();
+            sales01.ExtendSuccessSaleRevenue(1500);
+            Console.WriteLine("Salary: {0}", sales01.GetSalary());
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ g4/Workshop/Code/CompanyLibrary/Enums/RoleType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyLibrary.Enums
+{
+    public enum RoleType
+    {
+        Sales,
+        Manager,
+        Other
+    }
+}
