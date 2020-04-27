6d5466f96eb83a06edb709eab52d2eff394d5de5
Dejan <djovanov92@gmail.com>
2020-04-27 20:59:01 +0200

    Class07 part 2 added

---
+++ g4/Class07/Class07Part02/LibraryApp/LibraryApp.Models/Book/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryApp.Models.Book
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public Genres Genre { get; set; }
+
+        public int Pages { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public string BookInfo()
+        {
+            return $"{Title} - {Author} {Pages} pages ({Genre})";
+        }
+    }
+}
---
+++ g4/Class07/Class07Part02/LibraryApp/LibraryApp.Models/User/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryApp.Models.User
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public Roles Role { get; set; }
+
+        public User()
+        {
+            Role = Roles.User;
+        }
+
+        public User(string fName, string lName, string uName, string pass)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Username = uName;
+            Password = pass;
+            Role = Roles.User;
+        }
+
+        public string GreetPerson(string message)
+        {
+            return $"Hello {FirstName} welcome to the Library App";
+        }
+
+        public void PrintFullName()
+        {
+            Console.WriteLine($"{FirstName} {LastName}");
+        }
+
+
+    }
+}
