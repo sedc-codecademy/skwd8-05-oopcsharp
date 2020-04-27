d0d4ebbe9b509592e6eb50a369e530a74d4e8d12
Dragan Gelevski <hello@dragan.codes>
2020-04-27 18:37:55 +0200

    Class 7 pt1 init

---
+++ g7/Class7-digital/Class07.Inheritance/Entities/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class07.Inheritance.Entities
+{
+	public class Animal
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public AnimalType Type { get; set; } // Unknow, Dog, Cat or Other
+		public void Eat()
+		{
+			Console.WriteLine($"The {Type} animal named {Name} is eating!");
+		}
+		public string PrintInfo()
+		{
+			return $"Id: {Id}, Animal: {Type}, Name: {Name}";
+		}
+		// Animal bob = new Animal(); -> Create an empty animal with the type Unknown
+		public Animal()
+		{
+			Type = AnimalType.Unknown;
+			Console.WriteLine("An animal is created!");
+		}
+		public Animal(int id, string name, AnimalType type)
+		{
+			Id = id;
+			Name = name;
+			Type = type;
+			Console.WriteLine("An animal with data was created!");
+		}
+	}
+}
---
+++ g7/Class7-digital/Class07.Inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using Class07.Inheritance.Entities;
+using System;
+
+namespace Class07.Inheritance
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Animal bob = new Animal();
+			bob.Id = 12;
+			bob.Name = "Bob";
+			bob.Type = AnimalType.Dog;
+			bob.Eat();
+			Console.WriteLine(bob.PrintInfo());
+			Console.ReadLine();
+		}
+	}
+}
