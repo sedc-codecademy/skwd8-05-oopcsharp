cf6849676683d0e28136f582e92be85029a82985
dblazeski96 <dblazeski96@gmail.com>
2020-04-28 19:55:19 +0200

    Added code for inheritance

---
+++ g3/Class08/code/Domain/Bird.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Bird : Animal
+    {
+        public Bird()
+        {
+
+        }
+
+        public Bird(string eyeColor, string spcies, string wings, bool canSwim, bool canFly)
+        {
+            Spicies = spcies;
+            EyeColor = eyeColor;
+            CanSwim = canSwim;
+            Wings = wings;
+            //CanFly = true; // setting Default values
+            CanFly = canFly;
+        }
+
+        public string Wings { get; set; }
+        public bool CanFly { get; set; }
+
+    }
+}
