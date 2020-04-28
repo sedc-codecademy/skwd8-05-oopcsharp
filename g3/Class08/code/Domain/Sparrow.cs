5dbaf8c3cc625b4c11a15f66eeb987282ba4d172
dblazeski96 <dblazeski96@gmail.com>
2020-04-28 22:35:07 +0200

    Added comments for code of the class

--- g3/Class08/code/Domain/Sparrow.cs
+++ g3/Class08/code/Domain/Sparrow.cs
@@ -12,14 +12,14 @@ namespace Domain
             Console.WriteLine("");
         }
 
+        // : base(...) is the constructor of the Bird class, so by calling base() we are calling the constructor of Bird and we send some of the parameters that are passed in the Sparrow() constructor to the base constructor, not all parameters must be sent, also some of them can be sent by value only, like canSwim could be false directly
         public Sparrow(string spices, string eyeColor, string wings, string feathers, bool canFly) : base(eyeColor, spices, wings, false, canFly)
         {
-            //this.
+            // If we call the base constructor, we should not set the properties of the base class in this class, that's why they are commented here, and in the base constructor they are not commented
             //base.Spicies = spices;
             //base.EyeColor = eyeColor;
             //base.Wings = wings;
             this.Feathers = feathers;
-            //Sparrow sparrow = new Sparrow();
         }
     }
 }
