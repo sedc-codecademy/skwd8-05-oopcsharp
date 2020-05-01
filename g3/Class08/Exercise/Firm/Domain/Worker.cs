4bb7e628a1bf2d0f354cb0c93de4f36f57f32fab
Александар Коцевски <alek.kocevski@hotmail.com>
2020-05-01 19:06:56 +0200

    Class work + additional service methods

---
+++ g3/Class08/Exercise/Firm/Domain/Worker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Worker : Employee
+    {
+        //Properties
+        public Manager Manager { get; set; }
+        //Constructors
+        public Worker() : base()
+        {
+
+        }
+        public Worker(int id, string firstName, string lastName, Role role, int hours, double salary, Manager manager) : base(id,firstName,lastName,role,hours,salary)
+        {
+            Manager = manager;
+        }
+        //Methods
+        public string DoTask()
+        {
+            //string task = $"{FirstName} {LastName} is giving a task to {Id}";
+
+            return "task done";
+        }
+        public override string PrintInfo()
+        {
+            return $"Worker {FirstName} {LastName} is on {Salary} salary and has a manager {Manager}";
+        }
+    }
+}
