587cdc18b4f8555116acb50260b5483a5201986f
Dragan Gelevski <hello@dragan.codes>
2020-05-04 19:33:13 +0200

    Class 8 finito

--- g7/Class8-digital/Class08/Class08.GenericCollections/Program.cs
+++ g7/Class8-digital/Class08/Class08.GenericCollections/Program.cs
@@ -76,10 +76,35 @@ namespace Class08.GenericCollections
 			Console.ReadLine();
 			#endregion
 
-
+			#region Dictionary
 			// Dictionary
+			// WE MUST ADD 2 TYPES
+			// The first is KEY and the second is VALUE
+			// They both make a KEY VALUE PAIR
+			Dictionary<int, string> dictionaryPhonebook = new Dictionary<int, string>(); // empty dictionary
+			dictionaryPhonebook.Add(70444333, "Bob Bobsky"); // Add stuff
+			dictionaryPhonebook.Add(77123123, "Jill Wayne");
+			Dictionary<string, int> dictionaryPhonebookReverted = // initial values 
+				new Dictionary<string, int>()
+				{
+					{"Bob", 70444333},
+					{"Jill", 77123123}
+				};
+			// Because there is no key that is 0, it broke and it threw an Exception
+			// Console.WriteLine(dictionaryPhonebook[0]); // 0 is not INDEX, it is KEY
+			Console.WriteLine(dictionaryPhonebook[70444333]);
+			Console.WriteLine(dictionaryPhonebookReverted["Bob"]);
+			foreach (var item in dictionaryPhonebook)
+			{
+				Console.WriteLine($"KEY IS: {item.Key}"); // Takes the key out of one item
+				Console.WriteLine($"VALUE IS: {item.Value}");  // Takes the value out of one item
+			}
 
-			// Queue
+			Console.ReadLine();
+			#endregion
+
+			
+			// Queue ( First In First Out  - FIFO )
 			// [ 1, 2, 3, 4, 5 ]
 			// Actions: Get item, Add item, See what item is next
 			// WE CAN'T GET ANY ITEM AT ANY TIME
@@ -91,8 +116,27 @@ namespace Class08.GenericCollections
 			// WE MUST ADD ITEM AT THE END OF THE QUEUE
 			// In our case if we want to add the number 7 WE MUST DO IT LIKE THIS:
 			// [ 1, 2, 3, 4, 5, 7 ] - This is the only way
+			Queue<int> intQueue = new Queue<int>(); // Empty queue
+			intQueue.Enqueue(1); // Only method for adding items in queue
+			intQueue.Enqueue(2);
+			intQueue.Enqueue(3);
+			// intQueue[0]; // Can't get index items
+			// Dequeue removes the item that is next to go and returns it
+			int number = intQueue.Dequeue(); // number will get 1 but the queue will be [2, 3]
+			Console.WriteLine(number);
+			Console.WriteLine(intQueue.Peek()); // Just looks at the next item in line. DOES NOT TAKE IT OUT OF THE QUEUE
+			intQueue.Enqueue(4);
+			Console.WriteLine(intQueue.Peek());
+			// After peek the queue will still be the same [2, 3]
+			// We can peek as many times as we want as long as there is at least 1 item in the queue
+			// We can't dequeue as many times because at some point we will empty the queue completely
+			Console.ReadLine();
+			intQueue.Dequeue();
+			intQueue.Dequeue();
+			// intQueue.Dequeue(); This will break the queue because we can't dequeue an empty queue
 
-			// Stack
+
+			// Stack ( Last In First Out  - LIFO )
 			// [ 1, 2, 3, 4, 5 ]
 			// Actions: Get item, Add item, See what item is next
 			// WE CAN'T GET ANY ITEM AT ANY TIME
@@ -104,6 +148,19 @@ namespace Class08.GenericCollections
 			// WE MUST ADD ITEM AT THE END OF THE STACK
 			// In our case if we want to add the number 7 WE MUST DO IT LIKE THIS:
 			// [ 1, 2, 3, 4, 5, 7 ] - This is the only way
+			Stack<string> stringsStack = new Stack<string>(); 
+			stringsStack.Push("Bob"); // We add stuff in the Stack
+			stringsStack.Push("Jill");
+			stringsStack.Push("Greg"); // ["Bob","Jill","Greg"]
+			// stringsStack[0]; // This is broken since we can't use indexing
+			string name = stringsStack.Pop(); // Remove the Last thing that was added and add it to the name variable
+			// After the Pop: ["Bob","Jill"]
+			Console.WriteLine(name);
+			Console.WriteLine(stringsStack.Peek()); // Peek works exactly like queue just it looks for the last added
+			stringsStack.Push("Meti");
+			Console.WriteLine(stringsStack.Peek());
+			// If we use Pop multiple times and empty the stack we will get an error that the stack is empty
+			Console.ReadLine();
 		}
 	}
 }
