20ad1221b3b6b3c6b070faf6cd962903e82e288b
GK0208 <goce.kabov@yahoo.com>
2020-05-07 14:00:12 +0200

    added class 13 prep

---
+++ g1/Class13Prep/TeamSource/TeamSource/Enteties/Coach.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamSource.Enteties
+{
+    public class Coach
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ g1/Class13Prep/TeamSource/TeamSource/Enteties/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamSource.Enteties
+{
+    public class Team
+    {
+        public string Name { get; set; }
+        public string Arena { get; set; }
+        public Coach Coach { get; set; }
+        public List<Player> Players { get; set; }
+    }
+}
