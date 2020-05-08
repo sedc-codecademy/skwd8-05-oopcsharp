83c29101ecf7d04d026bdb8bc3986039ba6ccbc1
Martin <panovski.martin93@gmail.com>
2020-05-08 17:31:42 +0200

    Class 8 preparations added

---
+++ g2/Class8_Part2/SEDC.LINQ.Exercises/SEDC.LINQ/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using TeamSource.Helpers;
+
+namespace SEDC.LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var teams = TeamsDataBase.GetAllTeams();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ g2/Class8_Part2/SEDC.LINQ.Exercises/SEDC.LINQ/TeamSource/Enteties/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamSource.Enteties
+{
+    public class Player
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public Dictionary<string, float> PlayerStatistic { get; set; }
+
+    }
+}
