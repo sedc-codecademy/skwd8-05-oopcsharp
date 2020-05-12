2aec520c769d26f6374a6aa46b3fd320c923a868
Александар Коцевски <alek.kocevski@hotmail.com>
2020-05-12 21:53:10 +0200

    Class10 examples for Exceptions

---
+++ g3/Class10/ErrorHandling/Exceptions.Basic/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Exceptions.Basic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            ////Compile time errors
+            //var result = 10 / 0;
+            //Console.Writ("This will not be written");
+            //var number = 0;
+            //var result = 10 / number;
+            //Console.WriteLine($"The result {result}");
+
+            //try
+            //{
+            //    var number = 0;
+            //    var result = 10 / number;
+            //    Console.WriteLine($"The result {result}");
+            //}
+            //catch (NullReferenceException ex)
+            //{
+            //    throw ex;
+            //}
+            //catch (IndexOutOfRangeException ex)
+            //{
+            //    Console.WriteLine($"ERROR: An error like this happened: {ex.Message}");
+            //    //throw ex;
+            //}
+            //catch (Exception)
+            //{
+
+            //    throw;
+            //}
+
+            //Simple showing of try/catch/finally block and explanation of what happens when
+            try
+            {
+                Console.WriteLine("Exceptions........");
+
+                Console.WriteLine("This is the part of the code that throws Exceptions.");
+                throw new Exception("This will be our custom exception");
+                //This will be never executed because of the throw call!!!
+                Console.WriteLine("This will be never executed because of the throw call!!!");
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Exception or not, this will always be written");
+            }
+            Console.WriteLine("Application continues.... :)");
+
+            Console.ReadLine();
+        }
+    }
+}
