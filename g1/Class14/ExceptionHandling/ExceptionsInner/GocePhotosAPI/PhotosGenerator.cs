5d92dcbf4a0ba8f54af960a7a0ae728ad87fc6cc
Miodrag <miodrag.cekikj@seavus.com>
2020-05-12 23:05:35 +0200

    Class 14 code - Exception Handling Mechanism

---
+++ g1/Class14/ExceptionHandling/ExceptionsInner/GocePhotosAPI/PhotosGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System;
+using ExceptionsInner.CustomExceptions;
+
+namespace ExceptionsInner.GocePhotosAPI
+{
+    public class PhotosGenerator
+    {
+        public static List<string> GetAllImagesWithLake()
+        {
+
+            try
+            {
+                // Code logic for retrieving all photos with lake background
+
+                string exceptionMessageCode = "10-13i2-2kdiajdsoiej9q9ew10wjdsaidiajwdasd";
+
+                throw new Exception($"Critical error happened with code {exceptionMessageCode}");
+
+                //return new List<string>() { "image1Lake.jpg", "image2Lake2.png" };
+            }
+            catch (Exception ex)
+            {
+                throw new GoceAPIException("Error happened when retrieving all lake photos in GoceAPI", ex);
+            }
+        }
+    }
+}
