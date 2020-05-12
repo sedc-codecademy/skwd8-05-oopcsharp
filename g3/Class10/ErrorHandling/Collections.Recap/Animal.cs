ae42404e0dd5d4f867e11759ee5f2f9ce408e07e
dblazeski96 <dblazeski96@gmail.com>
2020-05-12 18:30:38 +0200

    Added code for recap

---
+++ g3/Class10/ErrorHandling/Collections.Recap/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections.Recap
+{
+    public class Vehicle
+    {
+        public Brand Brand { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+    }
+
+    public enum Brand
+    {
+        Fiat101,
+        Yugo,
+        Fico,
+        Audi = 100,
+        Opel = 99
+    }
+}
