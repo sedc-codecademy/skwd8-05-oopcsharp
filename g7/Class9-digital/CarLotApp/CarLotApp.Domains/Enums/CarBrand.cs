8f05686dab2c782cc72d4e6dc32702db7a008499
dblazeski96 <dblazeski96@gmail.com>
2020-05-13 20:01:29 +0200

    Added CarLot App and comments for Exceptions pt2

---
+++ g7/Class9-digital/CarLotApp/CarLotApp.Domains/Enums/CarBrand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotApp.Domains.Enums
+{
+    public enum CarBrand
+    {
+        Audi,
+        Alfa,
+        BMW,
+        Bugatti,
+        Mercedes,
+        Renault
+    }
+}
--- g7/Class9-digital/SEDC.Class09/ExceptionHandlingPart2/Program.cs
+++ g7/Class9-digital/SEDC.Class09/ExceptionHandlingPart2/Program.cs
@@ -28,38 +28,38 @@ namespace ExceptionHandlingPart2
 
             try
             {
-                number = int.Parse(userInput);
+                number = int.Parse(userInput); // This line could potentially throw FormatException
 
-                if (number >= 10 && number <= 20)
+                if (number >= 10 && number <= 20) // This logics checks if the valid number that the user entered is between 10-20, if it's not it will throw the Damjan exception
                 {
                     Console.WriteLine("The number you entered is between 10-20");
                 }
                 else
                 {
-                    throw new DamjanException("The number you entered is NOT between 10-20");
+                    throw new DamjanException("The number you entered is NOT between 10-20"); // This will 100% throw FormatException, using the throw keyword
                 }
 
-                //Animal animal = animals.FirstOrDefault(a => a.Name == "Stojan");
-                //Console.WriteLine(animal.Name);
+                Animal animal = animals.FirstOrDefault(a => a.Name == "Stojan");
+                Console.WriteLine(animal.Name); // This line could throw NullReferenceException
             }
-            catch (FormatException ex)
+            catch (FormatException ex) // This catch is the handling of all FormatException errors that could happen
             {
                 Console.WriteLine($"{userInput} is not a number. {ex.Message}");
                 number = 0;
             }
-            catch (NullReferenceException ex)
+            catch (NullReferenceException ex) // This catch is the handling of all NullReferenceException errors that could happen
             {
                 Console.WriteLine($"There was an NullRefereceException {ex.Message} {ex.StackTrace}");
             }
-            catch (DamjanException ex)
+            catch (DamjanException ex) // This catch is the handling of all DamjanException errors that could happen
             {
                 Console.WriteLine($"Damjan Exception happend: {ex.Message}");
             }
-            catch (Exception ex)
+            catch (Exception ex) // This catch is the handling of all other exception, similar like what default is doing in switch-case scenario
             {
                 Console.WriteLine($"ERROR happend: {ex.Message}");
             }
-            finally
+            finally // This block will always be excecuted no matter what (success or fail)
             {
                 Console.WriteLine("I will be execetuted no matter what");
             }
