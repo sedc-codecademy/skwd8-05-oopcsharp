a4df33a262e2a890bc893827ffde69a3376be2a2
Risto Panchevski <Risto.Pancevski@stas.be>
2020-05-14 20:12:13 +0200

    Exceptions

---
+++ g5/Class09/Class09/MyException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Class09
+{
+    public class MyException : Exception
+    {
+        public MyException()
+        {
+            
+        }
+        public MyException(string message) : base(message)
+        {
+            
+        }
+    }
+}
---
+++ g5/Class09/WhileLoopExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace WhileLoopExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> accountNumbers = new List<string>() {"111", "222", "333"};
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Dobredojdovte vo marketot!");
+                    Console.WriteLine("Vnesete broj na karticka:");
+                    string account = Console.ReadLine();
+
+                    if (!accountNumbers.Contains(account))
+                    {
+                        throw new Exception("Vnesovte nevalidna karticka!");
+                    }
+
+                    Console.WriteLine($"{account}: Pocnete so kupuvanje");
+                    Thread.Sleep(5000);
+                    Console.WriteLine($"{account} Vi blagodarime!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+            }
+        }
+    }
+}
