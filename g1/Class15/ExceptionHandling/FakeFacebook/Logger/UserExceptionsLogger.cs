5d5debe7e1ff08c8b2787333e222ca08742d85da
GK0208 <goce.kabov@yahoo.com>
2020-05-14 20:40:28 +0200

    added class 15

---
+++ g1/Class15/ExceptionHandling/FakeFacebook/Logger/UserExceptionsLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FakeFacebook.Logger
+{
+    public class UserExceptionsLogger
+    {
+        private string FilePath { get; set; } = @"C:\Users\Goce.Kabov\Desktop\ServerResponse\skwd8-05-oopcsharp\g1\Class15Prep\ExceptionHandling\ExceptionLogger.txt";
+        public void LogException(Exception ex)
+        {
+            // open conection to file
+            StreamWriter sw = new StreamWriter(FilePath);
+            // write in file
+            sw.WriteLine($"{ex.GetType().Name}  {ex.Message} {ex.StackTrace}");
+            // close connection to file
+            sw.Close();
+        }
+    }
+}
---
+++ g1/Class15/ExceptionHandling/FakeFacebook/Program.cs
@@ -0,0 +1,41 @@
+﻿using FakeFacebook.Logger;
+using System;
+using WebApi.Exceptions;
+using WebApi.Service;
+
+namespace FakeFacebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allUsers = UserService.GetAllUsers();
+            //foreach (var user in allUsers)
+            //{
+            //    Console.WriteLine(user.FirstName);
+            //}
+            try
+            {
+                // var userByID = UserService.GetUserById(4);
+                var usersFriends = UserService.GetAllUsersFriends(null);
+                foreach (var user in usersFriends)
+                {
+                    Console.WriteLine(user.FirstName);
+                }
+                // Console.WriteLine(userByID.FirstName);
+            }
+            catch (UserServiceException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                var logger = new UserExceptionsLogger();
+                logger.LogException(ex);
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("I proceed to work");
+            Console.ReadLine();
+        }
+    }
+}
