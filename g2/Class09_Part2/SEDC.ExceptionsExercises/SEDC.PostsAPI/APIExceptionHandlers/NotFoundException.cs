fa35715040da5ead5bb47a933c0d7c3fc1336137
Martin <panovski.martin93@gmail.com>
2020-05-15 23:57:55 +0200

    Class 9 part 2 added

---
+++ g2/Class09_Part2/SEDC.ExceptionsExercises/SEDC.PostsAPI/APIExceptionHandlers/NotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.PostsAPI.APIExceptionHandlers
+{
+    public class NotFoundException : Exception
+    {
+        public int StatusCode { get; set; }
+        public NotFoundException(string message, Exception innerException, int statusCode)
+            :base(message, innerException)
+        {
+            this.StatusCode = statusCode;
+        }
+    }
+}
---
+++ g2/Class09_Part2/SEDC.ExceptionsExercises/SEDC.PostsAPI/PostClient.cs
@@ -0,0 +1,57 @@
+﻿using SEDC.DataAccess;
+using SEDC.DomainModels;
+using SEDC.PostsAPI.APIExceptionHandlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SEDC.PostsAPI
+{
+    public class PostClient
+    {
+        public static List<Post> GetAll(string url)
+        {
+            try
+            {
+                if (url == "https://api.posts/posts/all")
+                {
+                    List<Post> allPosts = Db.Posts();
+                    return allPosts;
+                }
+                else
+                {
+                    throw new Exception("Something wrong happened!");
+                }
+            }
+            catch (Exception ex)
+            {
+                //TODO: Implement custom exception for handling bad requests
+                throw new BadRequestException("Bad request", ex.InnerException, 400);
+            }
+        }
+
+        public static Post Get(string url)
+        {
+            // First post url: https://api.posts/posts/1
+            try
+            {
+                int id = Convert.ToInt32(url.Split('/').ToList().TakeLast(1).FirstOrDefault());
+                Post post = Db.Posts().Where(x => x.Id == id).SingleOrDefault();
+
+                if (post != null)
+                {
+                    return post;
+                }
+                else
+                {
+                    throw new Exception($"No post with Id: {id} found!");
+                }
+            }
+            catch (Exception ex)
+            {
+                //TODO: Implement custom exception for page not found 
+                throw new NotFoundException("Resource not found", ex.InnerException, 404);
+            }
+        }
+    }
+}
