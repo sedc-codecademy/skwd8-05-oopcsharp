60504bdf67eff2a0f51b1e5a4c41f86d988c7363
Kristina Spasevska <spasevska.kristina87@gmail.com>
2020-05-18 20:08:19 +0200

    class 10 part 01 g4 added

---
+++ g4/Class10/Class10Part01/Eshop/ProductsService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eshop
+{
+    public static class ProductsService
+    {
+        // A C# static class is a class that can't be instantiated. 
+        // A static class can contain static members only. 
+        // You can‘t create an object for the static class.
+        // We do not use the NEW keyword.
+
+        public static List<Product> GetAllProducts()
+        {
+            return new List<Product>()
+            {
+                new Product("Bread", 28),
+                new Product("Milk", 50),
+                new Product("Beer", 34.50),
+                new Product("Nutella", 99.99),
+                new Product("Yoghurt", 60),
+                new Product("Flour", 25),
+                new Product("Sugar", 12),
+                new Product("Bacon", 76.32)
+            };
+        }
+    }
+}
---
+++ g4/Class10/Class10Part01/Eshop/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Eshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = ProductsService.GetAllProducts();
+
+            Dictionary<string, int> order = new Dictionary<string, int>();
+
+            Console.WriteLine("Welcome to our shop");
+            Console.WriteLine("Our products are:");
+            Console.WriteLine(" -------------------------- ");
+            Console.WriteLine("ID - Name - Price");
+
+            products.ForEach(prod => Console.WriteLine($" {prod.Id} - {prod.Name} - {prod.Price} MKD"));
+
+
+            while (true)
+            {
+
+                if (order.Count > 0)
+                {
+                    Console.WriteLine(" =================================== ");
+                    Console.WriteLine("Items in shopping cart:");
+                    Console.WriteLine("Name - Quantity");
+                    foreach (var item in order)
+                    {
+                        Console.WriteLine($"{item.Key} - {item.Value}");
+                    }
+                    Console.WriteLine(" =================================== ");
+                }
+
+                Console.WriteLine("Would you like to continue to buy anything? Y/N");
+                string userInputExit = Console.ReadLine().ToLower();
+                if (userInputExit == "n" || userInputExit != "y")
+                {
+                    break;
+                }
+
+                while (true)
+                {
+                    Console.WriteLine("Enter product id:");
+                    int productId = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter quantity:");
+                    int productQuantity = int.Parse(Console.ReadLine());
+
+                    var wantedProd = products.SingleOrDefault(x => x.Id == productId);
+                    Console.WriteLine(wantedProd != null ? $"Product: {wantedProd.Name}, Quantity: {productQuantity} added to cart!" : "No such product");
+                    if (wantedProd != null) {
+                        order.Add(wantedProd.Name, productQuantity);
+                    }
+                    Console.WriteLine("Would you like another order? Y/N");
+                    string userInputExitOrder = Console.ReadLine().ToLower();
+                    if (userInputExitOrder == "n" || userInputExitOrder != "y")
+                    {
+
+                        break;
+                    }
+                }
+            };
+            
+            
+
+
+            //Console.ReadLine();
+        }
+    }
+}
