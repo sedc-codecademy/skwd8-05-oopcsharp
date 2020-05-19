c9c944432c436f24d39f3c248fe5c8339b7312e6
Александар Коцевски <alek.kocevski@hotmail.com>
2020-05-19 21:17:18 +0200

    Comments and requirements added

---
+++ g3/Class11/CinemaApp/CinemaApp.Services/HelperService.cs
@@ -0,0 +1,38 @@
+﻿using CinemaApp.Domains;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace CinemaApp.Services
+{
+    public static class HelperService
+    {
+        public static void DisplayMoviesTitles(List<Movie> movies)
+        {
+            movies.ForEach(movie => Console.WriteLine(movie.Title));
+        }
+
+        public static int SelectCinema(string userInput)
+        {
+            int number = 0;
+            try
+            {
+                number = int.Parse(userInput);
+                if(number == 1 || number == 2)
+                {
+                    return number;
+                }
+                else
+                {
+                    throw new Exception("The number must be either 1 or 2!!!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("The inputmust be number and be either 1 or 2!!!");
+            }
+            return number;
+        }
+    }
+}
