51223df34db5a3adbb48576d968175a37c60a279
Miodrag <miodrag.cekikj@seavus.com>
2020-05-19 23:26:01 +0200

    Class 16 - Final Touch
    - Getters/Setters Under the Hood
    - Debbuging

---
+++ g1/FinalTouch/Market/Product.cs
@@ -0,0 +1,63 @@
+﻿namespace Market
+{
+    public class Product
+    {
+        // Field
+        //private int _price;
+
+        #region Get and Set methods
+        // The very early beginnings... :)
+
+        // Getter method - GET
+        //public int GetPrice()
+        //{
+        //    return _price;
+        //}
+
+        // Setter method - SET
+        //public void SetPrice(int value)
+        //{
+        //    _price = value;
+        //}
+        #endregion
+
+        #region Property
+        // Property
+        //public int Price
+        //{
+        //    get { return _price; }
+        //    set { _price = value; }
+        //}
+        #endregion
+
+        // Auto - implemented property => C# 
+
+        // Read - Write Property
+        //public int Price { get; set; }
+
+        //public int Price { get; } // Read only property
+
+        private int _price; // field
+        public int Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                if (value > 100)
+                    Discount = 0.1f;
+
+                _price = value;
+            }
+        }
+
+        public float Discount { get; private set; }
+
+        public Product()
+        {
+            Price = 100;
+        }
+    }
+}
---
+++ g1/FinalTouch/Market/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new Product();
+
+            product.Price = 100;
+            Console.WriteLine($"The price of the product is {product.Price}");
+            Console.WriteLine($"The discount of the product is {product.Discount}");
+
+            product.Price = 150;
+
+            //if (product.Price > 100)
+            //    product.Discount = 0.1f;
+
+            Console.WriteLine($"The price of the product is {product.Price}");
+            Console.WriteLine($"The discount of the product is {product.Discount}");
+
+            Console.ReadLine();
+        }
+    }
+}
