824e2891d671248d548a043239915f213fd7cbf6
dblazeski96 <dblazeski96@gmail.com>
2020-05-20 19:49:30 +0200

    Added code for class 10 part 2

---
+++ g7/class10-digital/ProgrammingTypes/StaticAndMoreErrorTypes/HelperService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticAndMoreErrorTypes
+{
+    public class HelperService
+    {
+        static HelperService()
+        {
+            nums = new List<int>();
+        }
+
+        public static List<int> nums { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("Something");
+        }
+
+        public static int TryParse(string userInput)
+        {
+            int num;
+            try
+            {
+                num = int.Parse(userInput);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"WARNING! Number parsing was no succesfull, insdead it was used 0");
+                num = 0;
+            }
+            return num;
+        }
+
+        public static void AddNumbers(string userNum1, string userNum2, Movie movie)
+        {
+            int num1 = 0, num2 = 0;
+
+            try
+            {
+                num1 = int.Parse(userNum1);
+                num2 = int.Parse(userNum2);
+                Console.WriteLine(movie.Name);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"You need to enter a valid string, not NULL {ex.Message}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"The conversion was not succesfulll {ex.Message}");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine($"The number is too large for a 32-bit value {ex.Message}");
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine($"Value is null {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Generic message {ex.Message}");
+            }
+        }
+    }
+}
---
+++ g7/class10-digital/ProgrammingTypes/StaticAndMoreErrorTypes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StaticAndMoreErrorTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelperService helperService = new HelperService();
+            helperService.Print();
+
+            HelperService.TryParse("34");
+            Math.Max(4, 4);
+            Math.Sqrt(5);
+
+            Movie movie = null;
+
+            if (3 < 2)
+            {
+                // Visual Studio gives us a warning for unreachable code
+                movie = new Movie { Name = "Terminator 2" };
+            }
+
+            HelperService.AddNumbers("34", "32", movie);
+            HelperService.AddNumbers("34", "3o", movie);
+            HelperService.AddNumbers(null, "32", new Movie { Name = "The Terminator" });
+
+            // Overflow
+            //int number = 7376473278463284687468764873684638764838746328487236487326473647828763874632432;
+
+            Console.WriteLine("Press any key to continue!");
+            Console.ReadKey();
+        }
+    }
+}
